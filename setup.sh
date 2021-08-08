52e5aa58b12969fa4972b9413d795eb0adbdd7e3
Soufiane Mourragui <soufiane.mourragui@free.fr>
2021-08-08 17:00:47 +0200

    setup changes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 # Activate Jupyter notebook
+conda create -n transact_figures python=3.6
+conda activate transact_figures
 pip install -r requirements.txt
 pip install ipykernel
+conda install rpy2
 python -m ipykernel install --user --name transact_figures --display-name "Python (TRANSACT_figures)"
 conda install tzlocal
+
+# Install edgeR
+conda install -c bioconda bioconductor-edger
+# python install_edgeR.py
 
 # Install TRANSACT
 pip install transact_dr
